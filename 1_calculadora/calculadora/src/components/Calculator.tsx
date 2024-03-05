551e9804c44471df9ef79420fef66bf81c3b1585
Lucas Cercal <lucascercal.fontes@gmail.com>
2024-03-05 13:52:09 -0300

    Estruturando o teclado

--- 1_calculadora/calculadora/src/components/Calculator.tsx
+++ 1_calculadora/calculadora/src/components/Calculator.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import "./Calculator.css";
+
 const Calculator = () => {
+  const keypadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const operations = ["+", "-", "*", "/"];
+
   return (
     <div className="calculator">
-      <div className="complete-operation">3 + 3 = 6</div>
-      <div className="display">6</div>
-      <div className="buttons"></div>
-      <button>=</button>
+      <div className="complete-operation"></div>
+      <div className="display"></div>
+      <div className="buttons">
+        <button>AC</button>
+        {keypadNumbers.map((number) => (
+          <button key={number}>{number}</button>
+        ))}
+        {operations.map((operation) => (
+          <button key={operation}>{operation}</button>
+        ))}
+        <button>=</button>
+      </div>
     </div>
   );
 };
